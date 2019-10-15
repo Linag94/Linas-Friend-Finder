288a027d76be9166410398bf344e639e454e29a6
Thelina Geogre <George.thelina@gmail.com>
2019-10-14 20:51:08 -0400

    created array of friends

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Colis",
+		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLuhOIzCSfRyZmvACzS1yuLMdYwbJyN9T2MHQ-zd8Ij7HcoEThHw",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Sammy",
+		photo: "https://vignette.wikia.nocookie.net/freshprince/images/c/c4/Lisawilkes.jpg/revision/latest?cb=20180625191408",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Trenice",
+		photo: "https://s.abcnews.com/images/Entertainment/140711_dvo_ent_freshprince0_16x9_992.jpg",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Mac",
+		photo: "http://www.wetpaint.com/wp-content/uploads/2017/03/jazz1.jpg",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Hannah",
+		photo: "http://storage.torontosun.com/v1/dynamic_resize/sws_path/suns-prod-images/1297940642778_ORIGINAL.jpg?size=490x",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
